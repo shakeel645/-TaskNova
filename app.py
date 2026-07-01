38ccd137691ccf04994cd5e21dfe3aac150508d7
Shaik Shakeel <yourgmail@gmail.com>
2026-07-01 18:57:29 +0530

    Ready for Render deployment

--- app.py
+++ app.py
@@ -110,5 +110,8 @@ def delete_task(id):
 # ===========================
 # Run Server
 # ===========================
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
